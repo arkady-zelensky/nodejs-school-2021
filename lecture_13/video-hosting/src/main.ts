8c667bf29e1f5db9a3b67b6c27f16fbb7ded1de7
Arkady Zelensky <arkady.zelensky@yalantis.com>
2021-10-21 02:20:42 +0300

    changed port

--- lecture_13/video-hosting/src/main.ts
+++ lecture_13/video-hosting/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
