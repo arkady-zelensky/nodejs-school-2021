4d39e94ae2eecc2d80b428b67c3ae75047444442
Arkady Zelensky <arkady.zelensky@yalantis.com>
2021-10-20 21:23:17 +0300

    fixed action

--- lecture_13/video-hosting/src/shared/config.service.spec.ts
+++ lecture_13/video-hosting/src/shared/config.service.spec.ts
@@ -2,7 +2,6 @@ import {ConfigService} from "./config.service";
 
 describe('ConfigService', () => {
   it('should be defined', () => {
-    process.env.PG_HOST = 'localhost';
     const result = new ConfigService(process.env);
 
     expect(result).toBeDefined();
