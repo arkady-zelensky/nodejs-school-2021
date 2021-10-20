32c2c7b37922e23eef8f044094e75fd30265fcca
Arkady Zelensky <arkady.zelensky@yalantis.com>
2021-10-20 21:21:55 +0300

    updated tests

--- lecture_13/video-hosting/src/shared/config.service.spec.ts
+++ lecture_13/video-hosting/src/shared/config.service.spec.ts
@@ -2,6 +2,7 @@ import {ConfigService} from "./config.service";
 
 describe('ConfigService', () => {
   it('should be defined', () => {
+    process.env.PG_HOST = 'localhost';
     const result = new ConfigService(process.env);
 
     expect(result).toBeDefined();
